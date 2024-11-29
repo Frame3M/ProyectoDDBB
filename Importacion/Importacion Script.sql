62533894882e2c87340ce86d87f5c863ed23d040
AlbaQ33 <ortegamarco.it@gmail.com>
2024-11-29 02:30:14 -0300

    Update Importacion Script.sql

--- Importacion/Importacion Script.sql
+++ Importacion/Importacion Script.sql
@@ -39,13 +39,12 @@ CREATE OR ALTER PROCEDURE importacion.ConfExcelImport
 AS
 BEGIN
 	-- CONFIGURACION PARA CONSULTAS DISTRIBUIDAS (NECESARIO)
-	sp_configure 'show advanced options', 1;
-	RECONFIGURE;
-	GO
-	sp_configure 'Ad Hoc Distributed Queries', 1;
-	RECONFIGURE;
-	GO
-	--
+	EXEC('sp_configure ''show advanced options'', 1')
+	RECONFIGURE
+	
+	EXEC('sp_configure ''Ad Hoc Distributed Queries'', 1')
+	RECONFIGURE
+	
 	-- CONFIGURACION PARA ARQUITECTURA OLEDB (CONF PARA ERROR)
 	EXEC sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'AllowInProcess', 1;
 	EXEC sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'DynamicParameters', 1;
@@ -129,9 +128,13 @@ BEGIN
 	SELECT DISTINCT turno FROM #TempEmpleado te
 	WHERE turno IS NOT NULL AND NOT EXISTS (SELECT 1 FROM recursosHumanos.TurnoTrabajo t WHERE t.turno = te.turno)
 
+	--EN CASO DE QUE CUIL SEA NULL SE HACE EL CAMBIO POR UN CUIL GENERICO
+	UPDATE #TempEmpleado SET
+		cuil = '20-22222222-3' WHERE cuil IS NULL; --nuevo
+
 	--CARGO TABLA QUE CONTIENE EMPLEADOS
-	INSERT INTO recursosHumanos.Empleado (legajo,nombre,apellido,dni,cuil,emailPer,emailEmp,direccion,idSucursal,idTurno,idCargo)
-	SELECT legajo,nombre,apellido,dni,cuil,emailPer,emailEmp,te.direccion,s.id,t.id,c.id
+	INSERT INTO recursosHumanos.Empleado (nombre,apellido,dni,cuil,emailPer,emailEmp,direccion,idSucursal,idTurno,idCargo)
+	SELECT nombre,apellido,dni,cuil,emailPer,emailEmp,te.direccion,s.id,t.id,c.id
 	FROM #TempEmpleado te
 	INNER JOIN recursosHumanos.TurnoTrabajo t ON t.turno = te.turno
 	INNER JOIN recursosHumanos.CargoTrabajo c ON c.cargo = te.cargo
@@ -142,7 +145,7 @@ BEGIN
 END
 GO
 
-CREATE OR ALTER PROCEDURE importacion.importarMedioDePago ( @path VARCHAR(300) )
+CREATE OR ALTER PROCEDURE importacion.importarMedioDePago ( @path VARCHAR(300) ) --VARIOS CAMBIOS
 AS
 BEGIN
 	CREATE TABLE #TempMedios(
@@ -215,7 +218,7 @@ BEGIN
 	VOID VARCHAR(100),
 	categoria VARCHAR(100),
 	nombre VARCHAR(100),
-	precio VARCHAR(100),
+	precioUSD VARCHAR(100),
 	precioRef VARCHAR(100),
 	unidadRef VARCHAR(100),
 	fecha VARCHAR(100))
@@ -241,19 +244,24 @@ BEGIN
 	END CATCH
 
 	UPDATE #TempCatalogo SET
-		precio = CAST(precio AS DECIMAL(9,2)),
+		precioUSD = CAST(precioUSD AS DECIMAL(9,2)),
 		precioRef = CAST(precioRef AS DECIMAL(9,2)),
 		fecha = CONVERT(SMALLDATETIME,fecha);
 
+	--ARREGLO CARACTERES DAÑADOS SI LOS HUBIERA
+	UPDATE #TempCatalogo SET
+		nombre = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(nombre, 'Ã±', 'ñ')
+		, 'Ã³', 'ó'), 'Ã©', 'é'), 'Ã¡', 'á'), 'Ãº', 'ú'), 'Ã­', 'í'), 'ÃƒÂº', 'ú'), 'Ã‘', 'Ñ'), '?', 'ñ'), 'Ã‘', 'Ñ'), 'Âº' , 'º'), 'å˜', 'ñ'),'Ã','Á');
+
 	--SE ELIMINAN COPIAS QUE CONTENGA LA TABLA TEMPORAL
-	WITH Duplicados AS ( SELECT ROW_NUMBER() OVER(PARTITION BY nombre,precio ORDER BY nombre,precio ASC) AS COPIA,nombre,precio FROM #TempCatalogo)
+	WITH Duplicados AS ( SELECT ROW_NUMBER() OVER(PARTITION BY nombre,precioUSD ORDER BY nombre,precioUSD ASC) AS COPIA,nombre,precioUSD FROM #TempCatalogo)
 	DELETE FROM Duplicados WHERE COPIA > 1
 
 	--CARGO LA TABLA QUE CONTIENE PRODUCTOS
-	INSERT INTO catalogos.Producto (nombre,precio,precioRef,unidadRef,fecha,idCategoria)
-	SELECT nombre,precio,precioRef,unidadRef,fecha,c.id FROM #TempCatalogo t
+	INSERT INTO catalogos.Producto (nombre,precioUSD,precioRef,unidadRef,fecha,idCategoria)
+	SELECT nombre,precioUSD,precioRef,unidadRef,fecha,c.id FROM #TempCatalogo t
 	INNER JOIN catalogos.CategoriaProducto c ON c.categoria = t.categoria
-	WHERE nombre IS NOT NULL AND NOT EXISTS (SELECT 1 FROM catalogos.Producto p WHERE p.nombre = t.nombre AND p.precio = t.precio);
+	WHERE nombre IS NOT NULL AND NOT EXISTS (SELECT 1 FROM catalogos.Producto p WHERE p.nombre = t.nombre AND p.precioUSD = t.precioUSD);
 
 	DROP TABLE #TempCatalogo
 END
@@ -282,6 +290,11 @@ BEGIN
 	UPDATE #TempAccElect SET
 		precioUSD = CAST(precioUSD AS DECIMAL(9,2));
 
+	--ARREGLO CARACTERES DAÑADOS SI LOS HUBIERA
+	UPDATE #TempAccElect SET
+		nombre = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(nombre, 'Ã±', 'ñ')
+		, 'Ã³', 'ó'), 'Ã©', 'é'), 'Ã¡', 'á'), 'Ãº', 'ú'), 'Ã­', 'í'), 'ÃƒÂº', 'ú'), 'Ã‘', 'Ñ'), '?', 'ñ'), 'Ã‘', 'Ñ'), 'Âº' , 'º'), 'å˜', 'ñ'),'Ã','Á');
+
 	--SE ELIMINAN DUPLICADOS QUE CONTENGA LA TABLA TEMPORAL
 	WITH Duplicados AS ( SELECT ROW_NUMBER() OVER(PARTITION BY nombre,precioUSD ORDER BY nombre,precioUSD ASC) AS COPIA,nombre,precioUSD FROM #TempAccElect)
 	DELETE FROM Duplicados WHERE COPIA > 1
@@ -300,7 +313,7 @@ BEGIN
 	--CARGO LA TABLA QUE CONTIENE PRODUCTOS
 	INSERT INTO catalogos.Producto (nombre,precioUSD,idCategoria)
 	SELECT nombre,precioUSD,@idCat FROM #TempAccElect t
-	WHERE NOT EXISTS (SELECT 1 FROM catalogos.Producto p WHERE p.nombre = t.nombre AND p.precioUSD = t.precioUSD)
+	WHERE nombre IS NOT NULL AND NOT EXISTS (SELECT 1 FROM catalogos.Producto p WHERE p.nombre = t.nombre AND p.precioUSD = t.precioUSD)
 
 	DROP TABLE #TempAccElect
 END
@@ -315,7 +328,7 @@ BEGIN
 		proveedor VARCHAR(100),
 		categoria VARCHAR(100),
 		cantidadXun VARCHAR(100),
-		precioUn VARCHAR(100))
+		precioUSD VARCHAR(100))
 
 	DECLARE @query NVARCHAR(MAX)
 	SET @query = 'INSERT INTO #TempProdImport SELECT * 
@@ -332,10 +345,15 @@ BEGIN
 
 	--CAMBIO LOS TIPO DE DATO
 	UPDATE #TempProdImport SET
-		precioUn = CAST(precioUn AS DECIMAL(9,2));
+		precioUSD = CAST(precioUSD AS DECIMAL(9,2));
+
+	--ARREGLO CARACTERES DAÑADOS SI LOS HUBIERA
+	UPDATE #TempProdImport SET
+		nombre = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(nombre, 'Ã±', 'ñ')
+		, 'Ã³', 'ó'), 'Ã©', 'é'), 'Ã¡', 'á'), 'Ãº', 'ú'), 'Ã­', 'í'), 'ÃƒÂº', 'ú'), 'Ã‘', 'Ñ'), '?', 'ñ'), 'Ã‘', 'Ñ'), 'Âº' , 'º'), 'å˜', 'ñ'),'Ã','Á');
 
 	--SE ELIMINAN DUPLICADOS QUE CONTENGA LA TABLA TEMPORAL
-	WITH Duplicados AS ( SELECT ROW_NUMBER() OVER(PARTITION BY nombre,precioUn ORDER BY nombre,precioUn ASC) AS COPIA,nombre,precioUn FROM #TempProdImport)
+	WITH Duplicados AS ( SELECT ROW_NUMBER() OVER(PARTITION BY nombre,precioUSD ORDER BY nombre,precioUSD ASC) AS COPIA,nombre,precioUSD FROM #TempProdImport)
 	DELETE FROM Duplicados WHERE COPIA > 1
 
 	--CARGO LA TABLA QUE CONTIENE CATEGORIAS
@@ -343,10 +361,10 @@ BEGIN
 	SELECT DISTINCT categoria FROM #TempProdImport t WHERE NOT EXISTS (SELECT 1 FROM catalogos.CategoriaProducto c WHERE c.categoria = t.categoria)
 
 	--CARGO LA TABLA QUE CONTIENE PRODUCTOS
-	INSERT INTO catalogos.Producto (nombre,proveedor,cantXUn,precio,idCategoria)
-	SELECT nombre,proveedor,cantidadXun,precioUn,c.id FROM #TempProdImport t
+	INSERT INTO catalogos.Producto (nombre,proveedor,cantXUn,precioUSD,idCategoria)
+	SELECT nombre,proveedor,cantidadXun,precioUSD,c.id FROM #TempProdImport t
 	INNER JOIN  catalogos.CategoriaProducto c ON c.categoria = t.categoria
-	WHERE NOT EXISTS (SELECT 1 FROM catalogos.Producto p WHERE p.nombre = t.nombre AND p.precio = t.precioUn)
+	WHERE nombre IS NOT NULL AND NOT EXISTS (SELECT 1 FROM catalogos.Producto p WHERE p.nombre = t.nombre AND p.precioUSD = t.precioUSD)
 
 	DROP TABLE #TempProdImport
 END
@@ -400,9 +418,10 @@ BEGIN
 		hora = CONVERT(TIME,hora,108),
 		empleadoLeg = CAST(empleadoLeg AS INT);
 
+	--ARREGLO CARACTERES DAÑADOS SI LOS HUBIERA
 	UPDATE #TempVentas SET
 		producto = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(producto, 'Ã±', 'ñ')
-		, 'Ã³', 'ó'), 'Ã©', 'é'), 'Ã¡', 'á'), 'Ãº', 'ú'), 'Ã­', 'í'), 'ÃƒÂº', 'ú'), 'Ã‘', 'Ñ') , 'Ã', 'Á'), '?', 'ñ'), 'Ã‘', 'Ñ'), 'Âº' , 'º'), 'å˜', 'ñ');
+		, 'Ã³', 'ó'), 'Ã©', 'é'), 'Ã¡', 'á'), 'Ãº', 'ú'), 'Ã­', 'í'), 'ÃƒÂº', 'ú'), 'Ã‘', 'Ñ'), '?', 'ñ'), 'Ã‘', 'Ñ'), 'Âº' , 'º'), 'å˜', 'ñ'),'Ã','Á');
 
 	--CARGAMOS LA TABLA QUE CONTIENE LOS TIPO DE CLIENTE
 	INSERT INTO clientes.TipoCliente (tipo)
@@ -415,26 +434,93 @@ BEGIN
 	WHERE NOT EXISTS (SELECT 1 FROM ventas.TipoFactura tf WHERE tf.tipo = t.tipoFactura)
 
 	--CARGAMOS LA TABLA QUE CONTIENE LAS FACTURAS
-	INSERT INTO ventas.Factura (idFactura,idTipoFactura,ciudad,idTipoCliente,generoCliente,fecha,hora,legajoEmp)
-	SELECT idFactura,tf.id,ciudad,tc.id,generoCliente,fecha,hora,empleadoLeg FROM #TempVentas t
+	INSERT INTO ventas.Factura (idFactura,idTipoFactura,ciudad,idTipoCliente,generoCliente,fecha,hora,legajoEmp,estado)
+	SELECT idFactura,tf.id,ciudad,tc.id,generoCliente,fecha,hora,empleadoLeg,'Pagada' FROM #TempVentas t
 	INNER JOIN ventas.TipoFactura tf ON tf.tipo = t.tipoFactura
 	INNER JOIN clientes.TipoCliente tc ON tc.tipo = t.tipoCliente
 	WHERE t.idFactura IS NOT NULL AND t.empleadoLeg IS NOT NULL AND NOT EXISTS (SELECT 1 FROM ventas.Factura vf WHERE vf.idFactura = t.idFactura)
-
+	
 	--CARGAMOS LA TABLA QUE CONTIENE LA RELACION N:N ENTRE PRODUCTO Y FACTURA (LISTA DE PRODUCTOS)
-	INSERT INTO ventas.ListaProducto (idFactura,idProducto,cantidad)
-	SELECT idFactura,p.id,cantidad FROM #TempVentas t
-	INNER JOIN catalogos.Producto p ON p.nombre = t.producto
-	WHERE idFactura IS NOT NULL AND cantidad IS NOT NULL
+
+	;WITH Precios AS (
+		SELECT id,nombre,precioARS,precioUSD FROM #TempVentas t
+		INNER JOIN catalogos.Producto p ON p.nombre = t.producto COLLATE SQL_Latin1_General_CP1_CI_AI)
+	UPDATE Precios SET precioARS = apis.dolarHoy(1) * precioUSD;
+
+	INSERT INTO ventas.ListaProducto (idFactura,idProducto,cantidad,subtotal)
+	SELECT idFactura,p.id,cantidad,precioARS*cantidad FROM #TempVentas t
+	INNER JOIN catalogos.Producto p ON p.nombre = t.producto AND p.precioUSD = t.precioUn
+	WHERE t.idFactura IS NOT NULL AND t.cantidad IS NOT NULL
 	AND NOT EXISTS (SELECT 1 FROM ventas.ListaProducto lp WHERE lp.idFactura = t.idFactura AND lp.idProducto = p.id AND lp.cantidad = t.cantidad)
 
 	--CARGAMOS LA TABLA QUE CONTIENE COMPROBANTES
-	 INSERT INTO ventas.Comprobante (idTipo,idFactura,idMedioPago)
-	 SELECT 1,idFactura,mp.id FROM #TempVentas t
-	 INNER JOIN ventas.MedioDePago mp ON mp.nombreING = t.medioDePago
+	 INSERT INTO ventas.Comprobante (idFactura,fecha,hora)
+	 SELECT idFactura,fecha,hora FROM #TempVentas t
 	 WHERE idFactura IS NOT NULL AND NOT EXISTS (SELECT 1 FROM ventas.Comprobante c WHERE c.idFactura = t.idFactura)
+
+	--CARGAMOS LA INFORMACION DE COMPROBANTES FACTURA
+	;WITH Montos AS (
+		SELECT idFactura,SUM(subtotal) AS total FROM ventas.ListaProducto GROUP BY idFactura
+	)
+
+	INSERT INTO ventas.ComprobanteFactura(id,monto,identificadorPago,idMedioPago)
+	SELECT c.id,total,identPago,mp.id FROM ventas.Comprobante c
+	INNER JOIN Montos m ON m.idFactura = c.idFactura
+	INNER JOIN #TempVentas t ON t.idFactura = c.idFactura
+	INNER JOIN ventas.MedioDePago mp ON mp.nombreING = t.medioDePago COLLATE SQL_Latin1_General_CP1_CI_AI OR mp.nombreESP = t.medioDePago COLLATE SQL_Latin1_General_CP1_CI_AI
+	WHERE c.idFactura IS NOT NULL AND NOT EXISTS (SELECT 1 FROM ventas.ComprobanteFactura cf WHERE cf.id = c.id)
 
 	DROP TABLE #TempVentas
 END
 GO
+
+
+--SELECT * FROM ventas.ListaProducto WHERE idFactura = '133-14-7229'
+
+/*
+EXEC importacion.ConfExcelImport
+
+EXEC importacion.importarSucursal 'E:\PROYECTODDBB\TP_integrador_Archivos\Informacion_complementaria.xlsx'
+
+EXEC importacion.importarEmpleado 'E:\PROYECTODDBB\TP_integrador_Archivos\Informacion_complementaria.xlsx'
+
+EXEC importacion.importarMedioDePago'E:\PROYECTODDBB\TP_integrador_Archivos\Informacion_complementaria.xlsx'
+
+EXEC importacion.importarClasificacion 'E:\PROYECTODDBB\TP_integrador_Archivos\Informacion_complementaria.xlsx'
+
+EXEC importacion.importarCatalogo 'E:\PROYECTODDBB\TP_integrador_Archivos\Productos\catalogo.csv'
+EXEC importacion.importarAccesoriosElectronicos 'E:\PROYECTODDBB\TP_integrador_Archivos\Productos\Electronic accessories.xlsx'
+EXEC importacion.importarProductosImportados 'E:\PROYECTODDBB\TP_integrador_Archivos\Productos\Productos_importados.xlsx'
+
+EXEC importacion.importarVentasRegistradas 'E:\PROYECTODDBB\TP_integrador_Archivos\Ventas_registradas.csv'
+
+
+SELECT * FROM sucursales.Sucursal
+
+SELECT * FROM recursosHumanos.TurnoTrabajo
+SELECT * FROM recursosHumanos.CargoTrabajo
+SELECT * FROM recursosHumanos.Empleado
+
+SELECT * FROM ventas.MedioDePago
+
+SELECT * FROM catalogos.LineaCategoria
+SELECT * FROM catalogos.CategoriaProducto
+SELECT * FROM catalogos.Producto order by precioUSD
+
+
+
+SELECT * FROM clientes.TipoCliente
+SELECT * FROM ventas.TipoFactura
+SELECT * FROM ventas.Factura
+
+SELECT * FROM ventas.TipoComprobante
+
+SELECT * FROM ventas.Comprobante
+SELECT * FROM ventas.MedioDePago
+SELECT * FROM ventas.ListaProducto where idFactura = '187-83-5490'
+
+SELECT * FROM ventas.ComprobanteFactura
+SELECT c.id,idFactura,fecha,hora,monto,identificadorPago,idMedioPago FROM ventas.Comprobante c INNER JOIN ventas.ComprobanteFactura cf ON c.id = cf.id
+
+*/
 
